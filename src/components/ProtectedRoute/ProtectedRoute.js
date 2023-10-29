b3904eb6df9d3ca74e641775553a4149da309b21
Evgeniy Fedotov <NorthSky-Evg@yandex.ru>
2023-10-29 23:01:48 +0300

    for review

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import Preloader from "../Preloader/Preloader";
 
 export const ProtectedRoute = ({ loading, element: Component, ...props }) => {
-	if (loading) {
-		return <Preloader />
-	}
 	return (
-		props.loggedIn ? <Component {...props} /> : <Navigate to='/' replace />
-	)
-}
+		loading ? null : (
+			props.loggedIn ? <Component {...props} /> : <Navigate to="/" replace />
+		)
+	);
+};
